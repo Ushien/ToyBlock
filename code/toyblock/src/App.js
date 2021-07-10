e4d315ed49864230b1aaea3444b3822f9c9e25e8
Ushien <antoinevanaelst@hotmail.com>
2021-07-10 15:48:35 +0200

    Project initialization
    Setup of the first directories and the React structure, with the help of Create React App

---
+++ code/toyblock/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Let's go
+          Est-ce que ça fonctionne ?
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
