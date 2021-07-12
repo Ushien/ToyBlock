876dcb4b51419244e074df1b3b2124316a7fa060
Ushien <antoinevanaelst@hotmail.com>
2021-07-12 11:51:14 +0200

    Addition of the first blocks and the basic structure of App.js

--- code/toyblock/src/App.js
+++ code/toyblock/src/App.js
@@ -1,24 +1,144 @@
 import './App.css';
 import React, { Component } from 'react'
 
-const moneyname = 'Infocoin';
+function sayHello() {
+  alert('You clicked me!');
+}
 
-class Test extends Component {
-  renderTest(j) {
-    const arraytest = [];
-    for (let i = 0; i < j; i++) {
-      arraytest.push(<div>Let's make some {this.props.moneyname}!</div>)
-    }
-    return arraytest;
+class HandlingBlock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {distance: 0};
   }
-  render(){
-    return this.renderTest(3)
+
+  render(){ 
+    if (this.state.distance === 0){
+      return (
+      <div> <BlocIntro1 /> 
+
+      <button onClick={() => {this.setState({distance: this.state.distance+1})}}> X </button> 
+      <div>{this.state.distance}</div>
+
+      </div>
+      )  
+    }
+    if (this.state.distance ===1){
+      return (
+      <div><BlocIntro2 />
+
+      <div>{this.state.distance}</div>
+      </div>
+      )
+    }
   }
 }
 
+class BlocIntro1 extends Component {
+  render(){
+    return <div>Au beau milieu de la forêt se trouve un village...</div>
+  }
+}
+
+class BlocIntro2 extends Component {
+  render(){
+    return <div>Peuplé de petits animaux sympathiques</div>
+  }
+}
+
+class Bloc1 extends Component { //Notation des transactions
+  
+  render(){
+    return (
+      <div>
+        Bloc 1
+      </div>
+    );
+  }
+}
+
+
+class Bloc2 extends Component { //Décentralisation de la monnaie
+  
+  render(){
+    return (
+      <div>
+        Bloc 2
+      </div>
+    );
+  }
+}
+
+class Bloc3 extends Component { //Signature électronique
+  
+  render(){
+    return (
+      <div>
+        Bloc 3
+      </div>
+    );
+  }
+}
+
+class Bloc4 extends Component { //Listes d'attentes
+  
+  render(){
+    return (
+      <div>
+        Bloc 4
+      </div>
+    );
+  }
+}
+
+class Bloc5 extends Component { //Blockchain
+  
+  render(){
+    return (
+      <div>
+        Bloc 5
+      </div>
+    );
+  }
+}
+
+class Bloc6 extends Component { //Proof of work
+  
+  render(){
+    return (
+      <div>
+        Bloc 6
+      </div>
+    );
+  }
+}
+
+class Bloc7 extends Component { //Rétribution
+  
+  render(){
+    return (
+      <div>
+        Bloc 7
+      </div>
+    );
+  }
+}
+
+class Bloc8 extends Component { //Travail d'équipe
+  
+  render(){
+    return (
+      <div>
+        Bloc 8
+      </div>
+    );
+  }
+}
+
+const moneyname = 'Infocoin';
+
 class App extends Component {
   render() {
-    return <Test moneyname={moneyname} />
+    return <HandlingBlock />
   }
 }
 
